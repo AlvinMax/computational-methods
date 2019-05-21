a027c9672052398bfdbdda5aab3bbe17b343343f
AlvinMax <earth.maxim@gmail.com>
2019-05-21 23:57:50 +0300

    task6 added

--- homework_pypy/hw2/task6.py
+++ homework_pypy/hw2/task6.py
@@ -2,12 +2,16 @@ import numpy as np
 
 
 def inverse_matrix(m):
-    return np.linalg.inv(m)
+    return np.linalg.inv(np.array(m)).tolist()
+
+
+def transpose_matrix(m):
+    return np.array(m).transpose().tolist()
 
 
 def matrix_rate(m):
-    return np.linalg.norm(m)
+    return np.linalg.norm(np.array(m))
 
 
-def conv(m):
+def condition_number(m):
     return matrix_rate(m) * matrix_rate(inverse_matrix(m))
