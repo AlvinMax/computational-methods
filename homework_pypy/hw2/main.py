af0cf2aae1a6466bd829f0cbc49ebbcc8de7fcdf
Artem Abramov <atem199700@gmail.com>
2019-05-22 00:53:33 +0300

    add tests

--- homework_pypy/hw2/main.py
+++ homework_pypy/hw2/main.py
@@ -8,8 +8,9 @@ def read_matrix(path):
 	return m
 
 eps = 0.00001
-m = read_matrix("./tests/test1.in")
+m = read_matrix("./tests/test3.in")
 
 (gr_res, gr_it) = task5.gradient_it(m, eps)
 print("".join(["Result==", str(gr_res), " with eps==", str(eps), " count of iteration==", str(gr_it)]))
 # print("Result== ", gr_res, " with eps==", eps, " count of iteration==", gr_it)
+print(task5.subst(m, gr_res))
