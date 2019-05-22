003f638be9306d47b5a11b15fdf967036e319703
Artem Abramov <atem199700@gmail.com>
2019-05-22 10:30:01 +0300

    add Gauss in main

--- homework_pypy/hw2/main.py
+++ homework_pypy/hw2/main.py
@@ -1,4 +1,6 @@
+import copy
 import task5
+import task1
 import utils
 
 def read_matrix(path):
@@ -10,8 +12,12 @@ def read_matrix(path):
 
 eps = 0.00001
 m = read_matrix("./tests/test3.in")
+m_g = copy.copy(m)
+
+gauss_res = task1.gauss(m_g)
+print("".join(["Gauss Result==", str(gauss_res)]))
 
 (gr_res, gr_it) = task5.gradient_it(m, eps)
-print("".join(["Result==", str(gr_res), " with eps==", str(eps), " count of iteration==", str(gr_it)]))
+print("".join(["Gradient Result==", str(gr_res), " with eps==", str(eps), " count of iteration==", str(gr_it)]))
 # print("Result== ", gr_res, " with eps==", eps, " count of iteration==", gr_it)
-print(utils.subst(m, gr_res))
+# print(utils.subst(m, gr_res))
