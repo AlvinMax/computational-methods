4954c2a2c100ec86e7b4351d37d8657d83cd27aa
AlvinMax <earth.maxim@gmail.com>
2019-05-22 11:24:30 +0300

    gilbert matrix added

--- homework_pypy/hw2/utils.py
+++ homework_pypy/hw2/utils.py
@@ -7,29 +7,39 @@ def inverse_matrix(m):
 
 
 def transpose_matrix(m):
-	return np.array(m).T.tolist()
+    return np.array(m).T.tolist()
 
 
 def split_at(m):
-	r = []
-	for l in m:
-		r.append(l.pop())
-	return (m, r)
+    r = []
+    for l in m:
+        r.append(l.pop())
+    return (m, r)
+
 
 def subst(A, x):
-	return [sum(e_x * e_r for e_x, e_r in zip(row_A, x)) for row_A in A]
+    return [sum(e_x * e_r for e_x, e_r in zip(row_A, x)) for row_A in A]
+
 
 def minus(b, r):
-	return [e_b - e_r for e_b, e_r in zip (b, r)]
+    return [e_b - e_r for e_b, e_r in zip(b, r)]
+
 
 def plus(b, r):
-	return [e_b + e_r for e_b, e_r in zip (b, r)]
+    return [e_b + e_r for e_b, e_r in zip(b, r)]
+
 
 def mul(k, l):
-	return [e_l * k for e_l in l]
+    return [e_l * k for e_l in l]
+
 
 def scalar_product(a, b):
-	return sum(e_a * e_b for e_a, e_b in zip(a, b))
+    return sum(e_a * e_b for e_a, e_b in zip(a, b))
+
 
 def norm(l):
-	return sqrt(sum(e_l * e_l for e_l in l))
+    return sqrt(sum(e_l * e_l for e_l in l))
+
+
+def gilbert_matrix(n):
+    return [[1.0 / (1.0 + i + j) for i in range(n)] for j in range(n)]
